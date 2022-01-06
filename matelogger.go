e363e1e35d5504c1f725862acd4c50e59c7989c7
ahatli <moonks830@gmail.com>
2022-01-06 11:58:57 +0900

    작성중이던 로그파일이 존재하고 로그를 작성하는 프로그램이 실행되었을 때 이전에 작성하던 로그파일에 로그를 작성하도록 수정 로그 이름 형식이 잘못된 문제점 수정

--- matelogger.go
+++ matelogger.go
@@ -94,8 +94,7 @@ func (l *LogFile) Rotate() error {
 	l.mu.Lock()
 	l.mu.Unlock()
 	// rename dest file if it already exists.
-	if _, err := os.Stat(filepath.Join(l.path, l.name+l.logDate+".log")); err == nil {
-
+	if _, err := os.Stat(filepath.Join(l.path, l.name+"_"+l.logDate+".log")); err == nil {
 		//이전에 생성된 같은 이름의 로그를 건너뛰기 위해 파일 검사
 		var name string
 		for {
@@ -107,20 +106,28 @@ func (l *LogFile) Rotate() error {
 		}
 
 		l.file.Close()
-		if err = os.Rename(filepath.Join(l.path, l.name+l.logDate+".log"), filepath.Join(l.path, name)); err != nil {
-			return err
+
+		if l.innerLogSize > (1024 * 1024 * l.limitLogSize) {
+			if err = os.Rename(filepath.Join(l.path, l.name+"_"+l.logDate+".log"), filepath.Join(l.path, name)); err != nil {
+				return err
+			}
 		}
 	}
 	l.logDate = time.Now().Format("2006-01-02")
 
-	l.innerLogSize = 0
-	file, err := os.OpenFile(filepath.Join(l.path, l.name+l.logDate+".log"), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	file, err := os.OpenFile(filepath.Join(l.path, l.name+"_"+l.logDate+".log"), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
 		return err
 	}
 	// switch dest file safely.
 
 	l.file = file
+
+	if info, err := l.file.Stat(); err == nil {
+		l.innerLogSize = int(info.Size())
+	} else {
+		l.innerLogSize = 0
+	}
 
 	return nil
 }
